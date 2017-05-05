b3dd8ff5325490d2dbc1c154c65362a65edaf07e
Jacob Knispel <knispeja@rose-hulman.edu>
2017-05-04 20:17:38 -0400

    Wrote up the bm25 algorithm

--- query.py
+++ query.py
@@ -1,13 +1,47 @@
-import os
+import os, math
+
+# Constants
+directory = './Presidents/edited'
+k1 = 1.5
+b = 0.75
 
 def bm25(query, documents):
-    # Get average document length
-    averageLength = 0
-    for docName, docText in documents:
-        averageLength += len(docText)
-    averageLength /= len(documents)
+    # Document names to scores
+    scored_documents = []
 
-    print(averageLength)
+    # Get average document length
+    average_length = 0
+    for docName, docText in documents:
+        average_length += len(docText)
+    average_length /= len(documents)
+
+    # Computing document name to word frequency
+    documents_containing = {} # items like word -> count
+    word_frequency = {} # items like (word, doc_name) -> frequency
+    for word in query:
+        for doc_name, doc_text in documents:
+            frequency = doc_text.count(word)
+            if word not in documents_containing:
+                documents_containing[word] = 0
+            if frequency > 0:
+                documents_containing[word] += 1
+
+            word_frequency[(word, doc_name)] = frequency
+
+    # Score each document
+    for doc_name, doc_text in documents:
+        idf_sum = 0
+        for word in query:
+            nq = documents_containing[word]
+            idf_sum += math.log(
+                (len(documents) - nq + 0.5)/
+                (nq + 0.5)
+            )
+        term_freq = word_frequency[(word, doc_name)]
+        score = idf_sum * (
+            (term_freq*(k1 + 1))/
+            (term_freq + k1*(1-b+b*(len(doc_text)/average_length)))
+        )
 
 
 def skip_bigrams(query, documents):
@@ -16,26 +50,25 @@ def skip_bigrams(query, documents):
 
 def split_nonalphanumeric(s):
     split_str = []
-    lastPos = 0
+    last_pos = 0
     pos = 0
     while pos < len(s):
         if not s[pos].isalnum():
-            if lastPos == pos:
-                lastPos += 1
+            if last_pos == pos:
+                last_pos += 1
             else:
-                split_str.append(s[lastPos:pos])
-                lastPos = pos + 1
+                split_str.append(s[last_pos:pos])
+                last_pos = pos + 1
         pos += 1
-    if lastPos != pos:
-        split_str.append(s[lastPos:pos])
+    if last_pos != pos:
+        split_str.append(s[last_pos:pos])
     return split_str
 
 
 query = raw_input("Enter your search query:\n")
 presidents = []
-directory = './Presidents/edited'
 for doc in os.listdir(directory):
     docText = split_nonalphanumeric(open(directory + "/" + doc, "r").read())
     presidents.append((doc, docText))
 
-bm25(query, presidents)
+bm25(split_nonalphanumeric(query), presidents)
